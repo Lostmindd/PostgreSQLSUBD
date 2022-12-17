2eefb83669f891b7dc06b323d8f9b1a64f38f4c6
Lostmindd <coffeebrain666@gmail.com>
2022-12-17 13:48:11 +0300

    starting

--- LoginWindow.py
+++ LoginWindow.py
@@ -1,35 +1,69 @@
 from tkinter import *
 import psycopg2
+import hashlib
+from functools import partial
 
-bg_color = "black"
 
-def create_window():
-    window = Tk()
-    window.title("Окно авторизации")
-    window.geometry('320x400')
-    window.resizable(width=False, height=False)
-    window["bg"] = bg_color
+class LoginWindow(object):
+    def __init__(self, connect_cursor):
+        self.connect_cursor = connect_cursor
+    bg_color = "black"
 
-    notice = Label(
-        text="вам нужно авторизоваться, чтобы получить доступ к БД!",
-        fg="white",
-        bg=bg_color,
-        width=32,
-        height=2,
-        justify=CENTER,
-        wraplength=320,
-        font=("Verdana", 12)
-    )
-    database = Entry()
-    user = Entry()
-    password = Entry()
-    host = Entry()
-    port = Entry()
+    def bd_enter(self, login, passw):
+        self.connect_cursor.execute("SELECT pass from auth where login = '"+ login.get() +"'")
+        password = self.connect_cursor.fetchall()
+        if len(password) == 0:
+            print('нет такого пароля')
+            return
 
-    notice.grid(row=1, column=0)
-    database.grid(row=2, column=0)
-    user.grid(row=3, column=0)
-    password.grid(row=4, column=0)
-    host.grid(row=5, column=0)
-    port.grid(row=6, column=0)
-    window.mainloop()
+        hash_pass = hashlib.sha1(passw.get().encode('utf-8'))
+        if password[0][0] == hash_pass.hexdigest():
+            print('Закрытие окна')
+        else:
+            print('Неверный пароль')
+            return
+
+
+    def create_window(self):
+        window = Tk()
+        window.title("Окно авторизации")
+        window.geometry('320x400')
+        window.resizable(width=False, height=False)
+        window["bg"] = self.bg_color
+
+        notice = Label(
+            text="Вход",
+            fg="white", bg=self.bg_color,
+            width=4, height=3,
+            justify=CENTER,
+            font=("Verdana", 28)
+        )
+        login_note = Label(
+            text="Пароль:",
+            fg="white", bg=self.bg_color,
+            width=32, height=2,
+            justify=CENTER,
+            font=("Verdana", 12)
+        )
+        pass_note = Label(
+            text="Логин:",
+            fg="white", bg=self.bg_color,
+            width=32, height=2,
+            justify=CENTER,
+            font=("Verdana", 12)
+        )
+        empty = Label(width=32, height=2, bg=self.bg_color)
+        login = Entry(width=24, font=("Verdana", 12))
+        password = Entry(width=24, font=("Verdana", 12))
+        enter_button = Button(text="Вход",
+                              width=8, font=("Verdana", 12),
+                              command=partial(self.bd_enter, login, password))
+
+        notice.grid(row=1, column=0)
+        login_note.grid(row=2, column=0)
+        login.grid(row=3, column=0)
+        pass_note.grid(row=4, column=0)
+        password.grid(row=5, column=0)
+        empty.grid(row=6, column=0)
+        enter_button.grid(row=7, column=0)
+        window.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import psycopg2
 import LoginWindow
+import hashlib
 
-LoginWindow.create_window()
+
+connection = psycopg2.connect(
+  database="kp0092_27",
+  user="st0092",
+  password="qwerty92",
+  host="172.20.8.18",
+  port="5432"
+)
+
+connect_cursor = connection.cursor()
 
 
+
+start_window = LoginWindow.LoginWindow(connect_cursor)
+start_window.create_window()
+
+
+connect_cursor.close()
